f1c6d3f05cc362d0fe736fab597a6f11676b4457
Ken <ken_serrano@Hotmail.com>
2021-04-03 23:46:47 -0400

    Started Links

--- landing-page/src/Components/Header/Header.js
+++ landing-page/src/Components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './Header.module.css';
+import Img1 from '../../logo.svg';
+
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,10 +9,20 @@ import Col from 'react-bootstrap/Col';
 
 const header = () => (
     <div className={classes.HeaderPage}>
-        <Container>
-            <Row className={classes.HeaderRow}>
+        <Container fluid>
+            <Row>
                 <Col>
-                    Hello
+                    <div>
+                        <a href="#"><img src={Img1} /></a>
+                        <div className={classes.Links}>
+                            <a href="#">About</a>
+                            <a href="#">Careers</a>
+                            <a href="#">Events</a>
+                            <a href="#">Products</a>
+                            <a href="#">Support</a> 
+                        </div>
+                    </div>
+                    
                 </Col>
             </Row>
         </Container>
